81b6a6af7d1ed678573d0f62bc8c68d981f1976a
Lahsaini Yazid <lahsainitv@gmail.com>
2026-07-14 17:58:54 +0100

    Added chunks for questions.json

---
+++ v0/chunking/chunking.py
@@ -0,0 +1,35 @@
+import json 
+from pathlib import Path 
+from langchain_community.document_loaders import PyPDFLoader 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+BASE_DIR=Path("/home/yazid/stage/data/Question_a_traiter/pdfs")
+all_pdf_paths=list(BASE_DIR.rglob("*.pdf"))
+print(f"found {len(all_pdf_paths)} pdfs")
+all_pages=[]
+for path in all_pdf_paths:
+    if path.parent==BASE_DIR:
+        doc_date="unassigned"
+    else:
+        doc_date=path.parent.name 
+    try :
+        loader=PyPDFLoader(str(path))
+        pages=loader.load()
+        for page in pages:
+            page.metadata["folder_date"]=doc_date
+            page.metadata["file_name"]=path.name
+        all_pages.extend(pages)
+    except Exception as e:
+        print(f"Failed to load {path.name}:{e}")
+
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=800,
+                                             chunk_overlap=100,
+                                             add_start_index=True)
+chunks=text_splitter.split_documents(all_pages)
+my_chunks=[{"page_content":chunk.page_content,
+            "metadata":chunk.metadata}for chunk in chunks ]
+with open("chunks_questions.json","w",encoding="utf-8") as f:
+    json.dump(my_chunks,f,ensure_ascii=False,indent=2)
+
+
+
+
