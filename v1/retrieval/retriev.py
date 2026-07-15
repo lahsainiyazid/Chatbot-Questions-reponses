aebea1498697380525d67af620c6321b1af3cf1b
Lahsaini Yazid <lahsainitv@gmail.com>
2026-07-15 14:58:46 +0100

    Added v1 looks like it has convincing results

---
+++ v1/retrieval/retriev.py
@@ -0,0 +1,49 @@
+import os 
+import json 
+from langchain_core.documents import Document 
+from langchain_huggingface import HuggingFaceEmbeddings 
+from langchain_community.retrievers import BM25Retriever 
+from sentence_transformers import CrossEncoder 
+from langchain_chroma import Chroma 
+from langchain_classic.retrievers import EnsembleRetriever
+from google import genai 
+key=os.environ.get("key")
+client=genai.Client(api_key=key)
+documents=[]
+embeddings=HuggingFaceEmbeddings(model_name="BAAI/bge-m3",model_kwargs={"device":"cpu"},encode_kwargs={"normalize_embeddings":True})
+db=Chroma(persist_directory="/home/yazid/stage/v1/embeddings+db/db_questions_1",embedding_function=embeddings,collection_name="db_questions_1")
+with open("/home/yazid/stage/v1/chunking/chunks_questions_v1.json","r",encoding="utf-8") as f:
+    data=json.load(f)
+for item in data:
+    text=f"""Content:{item.get("page_content","")}
+        """.strip()
+    documents.append(Document(page_content=text,
+                              metadata={"page":item.get("page",""),
+                                        "folder_date":item.get("folder_date",""),
+                                        "start_index":item.get("start_index","")}))
+bm25=BM25Retriever.from_documents(documents)
+bm25.k=10
+dense=db.as_retriever(search_kwargs={"k":10})
+hybrid=EnsembleRetriever(retrievers=[dense,bm25],weights=[0.3,0.7])
+Question=input("Entrez votre question")
+results=hybrid.invoke(Question)
+reranker=CrossEncoder("BAAI/bge-reranker-v2-m3")
+pairs=[(Question,doc.page_content) for doc in results]
+scores=reranker.predict(pairs)
+ranked_docs=[doc for _,doc in sorted(zip(scores,results),
+                                     key=lambda x:x[0],
+                                     reverse=True)]
+final_docs=ranked_docs[:3]
+context="\n\n".join([doc.page_content for doc in final_docs])
+prompt = f"""You are an expert assistant. Answer the QUESTION in Arabic, using ONLY the CONTEXT below.
+
+Strict rules:
+1. Factuality: Rely strictly on facts directly mentioned in the context. Do not assume, extrapolate, or use external knowledge.
+2. Transparency: If the answer is not in the context, reply exactly: "أنا آسف، ولكن السياق المقدم لا يسمح بالإجابة على هذا السؤال." Do not guess.
+3. Clarity: Be concise, precise, and organized in Arabic.
+
+CONTEXT: {context}
+QUESTION: {Question}"""
+response=client.models.generate_content(model="gemini-2.5-flash",contents=prompt)
+print(response.text)
+
